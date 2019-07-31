cf735c2bda775883b66859c5af967c21b8db21d6
Leihj <leihuijuan18607353919>
2019-07-31 18:17:58 +0800

    更新添加和投资模块

---
+++ TestCase/test_bidLoan.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @File    : test_bidLoan.PY
+# @Date    : 2019/7/31-12:01
+# @Author  : leihuijuan
+# @Emali   : huijuan_lei@163.com
---
+++ TestCase/test_invst.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @File    : test_invst.PY
+# @Date    : 2019/7/31-16:17
+# @Author  : leihuijuan
+# @Emali   : huijuan_lei@163.com
